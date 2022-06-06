6afc424984c16ac61423a616c491889f0f38dcfa
unknown <mikhailrivera12@gmail.com>
2022-06-06 17:35:54 +0200

    updated convert to y value function

--- client/src/simulation/utils.js
+++ client/src/simulation/utils.js
@@ -1,28 +1,19 @@
 const convertToYValue = (inputFloor) => {
-    const convertions = [
-        [20, 25],
-        [19, 50],
-        [18, 75],
-        [17, 100],
-        [16, 125],
-        [15, 150],
-        [14, 175],
-        [13, 200],
-        [12, 225],
-        [11, 250],
-        [10, 275],
-        [9, 300],
-        [8, 325],
-        [7, 350],
-        [6, 375],
-        [5, 400],
-        [4, 425],
-        [3, 450],
-        [2, 475],
-        [1, 500]
-    ];
-    const result = convertions.filter(floor => floor[0] === inputFloor)[0];
-    return result[1];
+    let result;
+    
+    const correspondingYValue = (21 - inputFloor) * 25;
+    const distanceFromBottom = inputFloor - 1;
+    const distanceFromTop = 20 - inputFloor;
+
+    if (distanceFromBottom > distanceFromTop) {
+        const difference = distanceFromBottom - distanceFromTop;
+        result = correspondingYValue + (difference * 25);
+    } else {
+        const difference = distanceFromTop - distanceFromBottom;
+        result = correspondingYValue - (difference * 25);
+    }
+    
+    return result;
 }
 
 export default convertToYValue;
