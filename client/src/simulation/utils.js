e6fd4b989b7da8b09a9378b1c66e02c9f9c85b77
unknown <mikhailrivera12@gmail.com>
2022-06-06 19:30:21 +0200

    proper solution for flipping floor values

--- client/src/simulation/utils.js
+++ client/src/simulation/utils.js
@@ -1,18 +1,14 @@
 const convertToYValue = (inputFloor) => {
     let result;
     
-    const correspondingYValue = (21 - inputFloor) * 25;
     const distanceFromBottom = inputFloor - 1;
     const distanceFromTop = 20 - inputFloor;
 
     if (distanceFromBottom > distanceFromTop) {
-        const difference = distanceFromBottom - distanceFromTop;
-        result = correspondingYValue + (difference * 25);
+        result = 25 + (distanceFromTop * 25);
     } else {
-        const difference = distanceFromTop - distanceFromBottom;
-        result = correspondingYValue - (difference * 25);
+        result = 500 - (distanceFromBottom * 25);
     }
-    
     return result;
 }
 
